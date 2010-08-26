b7e557b60e84324b0cd1094c604dc4b4ec6e614c
Nils Asmussen <nils@script-solution.de>
2010-08-26 11:07:16 +0000

    FWS-path changed

--- config/userdef.php
+++ config/userdef.php
@@ -15,7 +15,7 @@
 define('TDL_VERSION',								'TodoList v1.0');
 define('TDL_GENERAL_ERROR',					'Die Aktion ist fehlgeschlagen (Keine genaueren Informationen).');
 
-define('TDL_FWS_PATH',							'../PHPLib/');
+define('TDL_FWS_PATH',							'../FrameWorkSolution/');
 
 define('TDL_DB_CHARSET',						'latin1');
 
